11ec4caadc82437bb56e44c7df1cd4987f48de25
Jhowman037 <giovanesobral10@hotmail.com>
2026-03-09 23:54:53 -0300

    feat: add home routes and GetHomeData use case with dayjs integration

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -43,3 +43,25 @@ export const WorkoutPlanSchema = z.object({
 export const UpdateWorkoutSessionBodySchema = z.object({
   completedAt: z.iso.datetime(),
 });
+
+export const HomeDataSchema = z.object({
+  activeWorkoutPlanId: z.uuid(),
+  todayWorkoutDay: z.object({
+    workoutPlanId: z.uuid(),
+    id: z.uuid(),
+    name: z.string(),
+    isRest: z.boolean(),
+    weekDay: z.enum(WeekDay),
+    estimatedDurationInSeconds: z.number(),
+    coverImageUrl: z.url().optional(),
+    exercisesCount: z.number(),
+  }),
+  workoutStreak: z.number(),
+  consistencyByDay: z.record(
+    z.iso.date(),
+    z.object({
+      workoutDayCompleted: z.boolean(),
+      workoutDayStarted: z.boolean(),
+    }),
+  ),
+});
